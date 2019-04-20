24111ceb81ce6335af24b929c0a690439fdf59bd
Carlos <carlos.88.vinicio@gmail.com>
2019-04-20 18:01:59 +0200

    Fixing bugs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCc-Iq-ctmEoVcQbMvOvGQbh1HQdy1V1so',
+    authDomain: 'firechat-1cf53.firebaseapp.com',
+    databaseURL: 'https://firechat-1cf53.firebaseio.com',
+    projectId: 'firechat-1cf53',
+    storageBucket: 'firechat-1cf53.appspot.com',
+    messagingSenderId: '203864583075'
+  }
+};
